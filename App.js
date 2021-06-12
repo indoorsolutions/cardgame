a39d4b550fecd536ad5f9f5a3b10e8dd9408406c
indoorsolutions <60092521+indoorsolutions@users.noreply.github.com>
2021-06-12 03:52:12 +0200

    Stack navigation added

--- App.js
+++ App.js
@@ -2,15 +2,32 @@ import "react-native-gesture-handler";
 import React from "react";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { Tabs } from "./src/tabs/Tabs";
+import { TabsScreen } from "./src/screens/TabsScreen";
 import { Provider } from "react-redux";
 import store from "./src/store/store";
+import { createStackNavigator } from "@react-navigation/stack";
+import { UpdatePlayerScreen } from "./src/screens/UpdatePlayerScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer styles={styles}>
-        <Tabs />
+        <Stack.Navigator initialRouteName={"CardGame"}>
+          <Stack.Screen
+            name={"CardGame"}
+            component={TabsScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name={"UpdatePlayer"}
+            component={UpdatePlayerScreen}
+            options={{ title: "Update Player" }}
+          />
+        </Stack.Navigator>
       </NavigationContainer>
     </Provider>
   );
